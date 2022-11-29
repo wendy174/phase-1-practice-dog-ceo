c46bf16c91d8905ec72a858c6a718002d7a81826
wendy <mizukilee@gmail.com>
2022-11-28 23:11:59 -0500

    commit tree 66e2a8b455ff5f216ec6bf272e581fa162ad27fa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+ .then(res => res.json()) //takes out of json 
+ //.then(console.log)
+ .then(data => renderDogs(data));
+
+ const renderDogs = (data) => {
+    console.log(data.message) // data is an now an array 
+
+    data.message.forEach(dogUrl => {
+        const image = document.createElement('img')
+        image.src = dogUrl;
+        console.log(image) 
+        // now to put on the dom; grab the div 
+        const container = document.querySelector('#dog-image-container') 
+        container.appendChild(image)
+    })
+
+
+}
